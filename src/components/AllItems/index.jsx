a533a8dfeaf15f31856d96bc18cdae374b3cc90b
KobraMik <slavik433@gmail.com>
2022-11-24 17:56:00 +0300

    refactor: change class component (connect) to functional component with hooks (useSelector and useDispatch)

--- src/components/AllItems/index.jsx
+++ src/components/AllItems/index.jsx
@@ -1,83 +1,85 @@
-// import React, {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./AllItems.scss";
 import "../../styles/basic.scss";
 import PizzaCard from "./PizzaCard";
-
-// const AllItems = () => {
-//     const [pizzas, setPizzas] = useState([])
-//
-//     useEffect(() => {
-//         fetch("http://localhost:3000/db.json").then((res) => (res.json()))
-//             .then((res) => {
-//                 setPizzas(res.pizzas)
-//             })
-//     }, [])
-//
-//     return (
-//         <section className="all-items-block">
-//             <div className="container radius">
-//                 <div className="all-items">
-//                     <h2 className="all-items__title">
-//                         Все пиццы
-//                     </h2>
-//                     <div className="pizzas">
-//                         {
-//                             pizzas.map((item) => {
-//                                 return <PizzaCard key={item.id} {...item}/>
-//                             })
-//                         }
-//                     </div>
-//                 </div>
-//             </div>
-//         </section>
-//     );
-// };
-//
-// export default AllItems;
-//
-
-import React, {Component} from "react";
-import {connect} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {getPizzas} from "../../redux/actions/pizzasActions";
 
-class AllItems extends Component {
-    componentDidMount() {
+const AllItems = () => {
+    const items = useSelector(state => state.pizzas.items)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
         fetch("http://localhost:3000/db.json").then((res) => (res.json()))
             .then((res) => {
-                this.setState(this.props.getPizzas(res.pizzas))
+                dispatch(getPizzas(res.pizzas))
             })
-    }
+    }, [])
 
-    render() {
-        return (
-            <section className="all-items-block">
-                <div className="container radius">
-                    <div className="all-items">
-                        <h2 className="all-items__title">
-                            Все пиццы
-                        </h2>
-                        <div className="pizzas">
-                            {
-                                this.props.items.map((item) => {
-                                    return <PizzaCard key={item.id} {...item}/>
-                                })
-                            }
-                        </div>
+    return (
+        <section className="all-items-block">
+            <div className="container radius">
+                <div className="all-items">
+                    <h2 className="all-items__title">
+                        Все пиццы
+                    </h2>
+                    <div className="pizzas">
+                        {
+                            items.map((item) => {
+                                return <PizzaCard key={item.id} {...item}/>
+                            })
+                        }
                     </div>
                 </div>
-            </section>
-        );
-    }
-}
+            </div>
+        </section>
+    );
+};
 
+export default AllItems;
 
-const mapStateToProps = (state) => ({
-    items: state.pizzas.items,
-    // loading: state.pizzas.loading
-})
-
-const mapDispatchToProps = (dispatch) => ({
-    getPizzas: (items) => dispatch(getPizzas(items))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(AllItems);
+// import React, {Component} from "react";
+// import {connect} from "react-redux";
+// import {getPizzas} from "../../redux/actions/pizzasActions";
+//
+// class AllItems extends Component {
+//     componentDidMount() {
+//         fetch("http://localhost:3000/db.json").then((res) => (res.json()))
+//             .then((res) => {
+//                 this.setState(this.props.getPizzas(res.pizzas))
+//             })
+//     }
+//
+//     render() {
+//         return (
+//             <section className="all-items-block">
+//                 <div className="container radius">
+//                     <div className="all-items">
+//                         <h2 className="all-items__title">
+//                             Все пиццы
+//                         </h2>
+//                         <div className="pizzas">
+//                             {
+//                                 this.props.items.map((item) => {
+//                                     return <PizzaCard key={item.id} {...item}/>
+//                                 })
+//                             }
+//                         </div>
+//                     </div>
+//                 </div>
+//             </section>
+//         );
+//     }
+// }
+//
+//
+// const mapStateToProps = (state) => ({
+//     items: state.pizzas.items,
+//     loading: state.pizzas.loading
+// })
+//
+// const mapDispatchToProps = (dispatch) => ({
+//     getPizzas: (items) => dispatch(getPizzas(items))
+// })
+//
+// export default connect(mapStateToProps, mapDispatchToProps)(AllItems);
